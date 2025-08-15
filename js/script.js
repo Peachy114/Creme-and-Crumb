f11faa7a386cf1ffb84753a2c3423e8626dfe5b7
Fatma <gabuyafatma14@gmail.com>
2025-08-15 20:59:07 +0800

    fixed reponsive and DOM content

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-fetch('data/trending.json')
-  .then(response => response.json())
-  .then(data => {
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const res = await fetch('data/trending.json');
+    const data = await res.json();
+
     const container = document.getElementById('trends');
     const ul = document.createElement('ul');
-    ul.className = "grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-5 px-5 lg:px-0";
+    ul.className = "grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-5 px-5 mx-10";
+
     data.forEach(food => {
       ul.innerHTML += `
         <li class="flex flex-col mb-8">
@@ -16,5 +19,7 @@ fetch('data/trending.json')
     });
 
     container.appendChild(ul);
-  })
-  .catch(error => console.error('Error Loading JSON:', error));
+  } catch (err) {
+    console.error('Error Loading JSON:', err);
+  }
+});
